23d5425484b95f1462db9195d1cd8a432eed1fc1
James Ainooson <jainooson@gmail.com>
2018-10-12 12:07:44 -0500

    Initial commit with initial structure

---
+++ src/agents.py
@@ -0,0 +1,3 @@
+class RandomAgent(object):
+
+    def get_next_move(self, board_state):
---
+++ src/oware.py
@@ -0,0 +1,53 @@
+def four_four_rules(player, pit, board_state):
+    """
+    Play a turn of the four four rule game
+    :param player:
+    :param pit:
+    :param board_state:
+    :return:
+    """
+
+    if player == 'SOUTH' and pit > 5:
+        raise ValueError("Invalid pit number %d for south player. Pit number for south must be 0 through 5" % pit)
+    if player == 'NORTH' and pit < 6:
+        raise ValueError("Invalid pit number %d for north player. Pit number for north must be 6 through 11" % pit)
+    if player != 'SOUTH' and player !='NORTH':
+        raise ValueError("Player must be either north or south")
+
+    seeds_in_hand = board_state.pits[pit]
+    board_state.pits[pit] = 0
+
+    while seeds_in_hand > 0:
+        pit = pit + 1 % 12
+
+
+
+
+class BoardState(object):
+    """
+    Represents the state of a single board
+    """
+
+    def __init__(self, pits=None, score=None):
+        self.pits = pits if pits else [4] * 12
+        self.score = score if score else [0, 0]
+
+
+
+
+
+class Agent(object):
+    def play_turn(self, game_state):
+
+class Game(object):
+    """
+    A Game object represents a match taking place between two agents, one representing north and the other representing
+    south. This class manages the overall game state (which includes the board positions and scores), calls up agents to
+    play their turns and handles interaction with UI components that display the Game.
+    """
+
+    def __init__(self, state=None):
+        self.game_state = BoardState() if state is None else state
+
+
+
